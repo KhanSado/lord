b73ae0958b6048d22c7eea898fb4aa7918d16032
bersoncrios <bersondev@gmail.com>
2022-10-11 21:47:48 -0300

    add readme and License

---
+++ README.md
@@ -0,0 +1,73 @@
+[![](https://jitpack.io/v/karontyy/lord.svg)](https://jitpack.io/#karontyy/lord)
+
+# Welcome to Lord UI !
+Lord is library with UI components using jetpack compose
+⚠ **Under Construction**
+
+## Sample
+
+<img src="https://raw.githubusercontent.com/karontyy/lord/master/sample.GIF" width="220" height="340"/>
+
+# implementing
+Implementing mode
+
+### Download
+
+Add it in your root build.gradle at the end of repositories:
+```css
+allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+Add the dependency
+```css
+dependencies {
+	 implementation 'com.github.karontyy:lord:0.1.0'
+}
+```
+### Usage
+
+Create a array to slide informations, each slide is an object
+```kotlin
+val data = arrayOf(  
+    DataSlide(
+	    "Title slide one",
+	    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id.", 
+	    img = R.drawable.piplup
+    ),
+    DataSlide(
+	    "Title slide two",
+	    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id.", 
+	    img = R.drawable.chamander
+    )
+)
+```
+
+* Slide View Basic usage
+```kotlin
+SliderView(  
+   items = data  
+)
+```
+* Slide View full usage
+```kotlin
+SliderView(  
+    items = data,  
+    bgColor = Color.Gray, //define background color screen
+	colorTitle = Color.Cyan, // Define title color
+	colorDescription = Color.Green, // Define decription color
+	fontFamilyTitle = FontFamily.Monospace, //dfine title font family 
+	fontFamilyDescription = FontFamily.Serif // define dscription font family 
+)
+```
+
+## Show some ❤ and support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.buymeacoffee.com/karonty" target="_blank">
+    <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" width="160">
+</a>
--- app/src/main/java/com/br/lordui/MainActivity.kt
+++ app/src/main/java/com/br/lordui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import com.br.lord.slideview.DataSlide
 import com.br.lord.slideview.SliderView
@@ -18,14 +20,22 @@ class MainActivity : ComponentActivity() {
             DataSlide("Title slide one", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id.", img = R.drawable.piplup),
             DataSlide("title slide two", "Sed id velit non sapien laoreet sodales. Curabitur aliquet sem nunc, fringilla ultrices lacus iaculis id.", img = R.drawable.chamander),
             DataSlide("title slidde three", "Maecenas porttitor a metus sit amet venenatis.", img = R.drawable.sprigatito),
-            DataSlide("title slidde three", "Maecenas porttitor a metus sit amet venenatis.", img = R.drawable.sprigatito),
-            DataSlide("title slidde three", "Maecenas porttitor a metus sit amet venenatis.", img = R.drawable.sprigatito),
         )
         setContent {
             LorduiTheme {
+                //BASIC USAGE
                 SliderView(
-                    items = data
+                    items = data,
                 )
+                //FULL USAGE
+//                SliderView(
+//                    items = data,
+//                    bgColor = Color.Gray, //Definindo cor do fundo das telas
+//                    colorTitle = Color.Cyan, // Definindo cor do titulo
+//                    colorDescription = Color.Green, // Deinindo cor da descrição
+//                    fontFamilyTitle = FontFamily.Monospace, //Definindo Font family do titulo
+//                    fontFamilyDescription = FontFamily.Serif // Definindo Font family da descriçõa
+//                )
             }
         }
     }
